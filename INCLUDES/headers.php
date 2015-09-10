0d33c17d48f8656474bb8046ee6ea685d21d94e3
Alex <alextamayop@gmail.com>
2015-09-10 16:39:46 -0500

    archivos complementarios

---
+++ INCLUDES/headers.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- style for the specifict elements -->
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/navbar.css">
