e8f2cb5278a1704901d02bdfc42692b0bbc09dd0
Alxander <alextamayop@gmail.com>
2015-10-09 14:14:32 -0500

    changes in scripts and headers

--- INCLUDES/scripts.php
+++ INCLUDES/scripts.php
@@ -1,2 +1,5 @@
+<!-- SCRIPTS DE PROGRAMACIÓN -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 <script src="js/classie.js"></script>
 <script src="js/cbpAnimatedHeader.js"></script>
