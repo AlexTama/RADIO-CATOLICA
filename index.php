0f5c845c8ef80de8cb87ccaafc47df71f72a8273
Alxander <alextamayop@gmail.com>
2015-10-20 16:40:51 -0500

    changes in the footer style

--- index.php
+++ index.php
@@ -115,74 +115,57 @@
 	</section>
 
 	<!-- *************************************** -->
-	<!-- LAST SECTION -->
+	<!-- THIRD SECTION -->
 	<!-- localization by map and footer -->
 	<!-- *************************************** -->
 
-	<section id="contacto" class="container-fluid color-front">
-		<div class="row">
+	<section id="contacto" class="container-fluid" style="height:100%; width:100%; background-color:#0B2E6F;">
+		<div class="row"
 			<div class="col-md-12">
 
-				<!-- ******************************************************************* -->
+				<!-- ****************************************************************#*** -->
 				<!-- MAP SECTION -->
 				<!-- ******************************************************************* -->
 
-				<section class="containder-fluid">
-					<div class="row">
-						<div id="gmap" class="col-md-12">
-							<div id="gmap-draw">
-								<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d993.9346577470208!2d-75.6953255!3d4.8148793!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e388748c4380771%3A0xd6fa8947dd7fe055!2sCatedral+de+Nuestra+Se%C3%B1ora+de+la+Pobreza!5e0!3m2!1ses!2ses!4v1441902223982" width="100%" height="450px" frameborder="0" style="border:0" allowfullscreen></iframe>
-							</div>
-
-						</div>
+				<div id="gmap" class="col-md-12">
+					<div id="gmap-draw">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d993.9346577470208!2d-75.6953255!3d4.8148793!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e388748c4380771%3A0xd6fa8947dd7fe055!2sCatedral+de+Nuestra+Se%C3%B1ora+de+la+Pobreza!5e0!3m2!1ses!2ses!4v1441902223982" width="100%" height="400px" frameborder="0" style="border:0" allowfullscreen></iframe>
 					</div>
-				</section>
+
+				</div>
 				
 				<!-- ******************************************************************* -->
 				<!-- INFORMATION ABOUT THIS SITE -->
 				<!-- ******************************************************************* -->
 
-				<section class="containder-fluid" style="margin:2% 0;">
-					<div class="row">
+				<div class="col-md-6 col-md-offset-3">
 
-						<div class="col-md-6 col-md-offset-3 contact-style">
+					<!-- FOOTER -->
+					<div>
+						<hgroup>									
+							<h1 class="text-center color-font lead subtitle-size" style="letter-spacing:5px;">Contacto</h1>
+						</hgroup>
 
-							<!-- FOOTER -->
-							<div>
-								<hgroup>									
-									<h1 class="text-center color-font lead subtitle-size" style="letter-spacing:5px;">Contacto</h1>
-								</hgroup>
-
-								<!--*************************************************-->
-								<div class="col-md-6 color-font text-center">
-									<h4 style="letter-spacing:5px;"><b>DIRECCIÓN</b></h4>
-									<p class="lead" style="letter-spacing:5px;">Calle 20 # 7-40</p>
-								</div>
-								
-								<!--*************************************************-->
-								<div class="col-md-6 color-font text-center">
-									<h4 style="letter-spacing:5px;"><b>TEL</b></h4>
-									<p class="lead" style="letter-spacing:5px;">(6) 324 8484</p>
-								</div>
-							</div>
+						<!--*************************************************-->
+						<div class="col-md-6 color-font text-center">
+							<h4 style="letter-spacing:5px;"><b>DIRECCIÓN</b></h4>
+							<p class="lead" style="letter-spacing:5px;">Calle 20 # 7-40</p>
 						</div>
-
+								
+						<!--*************************************************-->
+						<div class="col-md-6 color-font text-center">
+							<h4 style="letter-spacing:5px;"><b>TEL</b></h4>
+							<p class="lead" style="letter-spacing:5px;">(6) 324 8484</p>
+						</div>
 					</div>
-				</section>
 
-			</div>
-		</div>
-	</section>
-	
-	<!-- *************************************** -->
-	<!-- FOOTER -->
-	<!-- copy right and credits by development -->
-	<!-- *************************************** -->
+				</div>
 
-	<section class="container-fluid">
-		<div class="row">
-			<div class="col-md-12 footer-style text-center">
-				<footer class="text-center footer-cp" style="letter-spacing:2px;">
+				<!-- *************************************** -->
+				<!-- FOOTER -->
+				<!-- copy right and credits by development -->
+				<!-- *************************************** -->
+				<footer class="text-center footer-cp color-font" style="letter-spacing:2px;">
 					<div class="col-md-6">
 						<p class="lead">© 2015 Radio Católica Pereira</p>
 					</div>
@@ -190,11 +173,11 @@
 						<p class="lead">Desarrollado por Alexander Tamayo Pino</p>
 					</div>
 				</footer>
+
 			</div>
 		</div>
 	</section>
-
-
+	
 	<!-- SCRIPTS FOR ANIMATION AND MAP -->
 	<?php include 'INCLUDES/scripts.php'; ?>
 </body>
